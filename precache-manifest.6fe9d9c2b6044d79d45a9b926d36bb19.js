23169af13be8c9a78ef042159de1dbbbd3a9b905
andy.xing <991376602@qq.com>
2021-05-15 22:47:27 +0800

    deploy

---
+++ precache-manifest.6fe9d9c2b6044d79d45a9b926d36bb19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87adccd8412a5fac0a02",
+    "url": "/moremoney-website/css/app.176bb9cb.css"
+  },
+  {
+    "revision": "2f0be52ff168c13731c978f4891aa737",
+    "url": "/moremoney-website/index.html"
+  },
+  {
+    "revision": "87adccd8412a5fac0a02",
+    "url": "/moremoney-website/js/app.9a8cdd68.js"
+  },
+  {
+    "revision": "b8a49482ebfc5970527a",
+    "url": "/moremoney-website/js/chunk-vendors.618a9fb4.js"
+  },
+  {
+    "revision": "766939830f19c118d854657571573aa6",
+    "url": "/moremoney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moremoney-website/robots.txt"
+  }
+]);
